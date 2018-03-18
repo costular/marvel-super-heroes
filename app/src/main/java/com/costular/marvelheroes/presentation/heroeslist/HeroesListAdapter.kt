e63dac51524621230d281c05ff628d6b92b6d293
Diego Francisco Concepción <diegooo.fc@gmail.com>
2018-03-18 13:56:33 +0100

    Add colors from imagery Apply some minor visual changes to hero item layout

--- app/src/main/java/com/costular/marvelheroes/presentation/heroeslist/HeroesListAdapter.kt
+++ app/src/main/java/com/costular/marvelheroes/presentation/heroeslist/HeroesListAdapter.kt
@@ -2,14 +2,22 @@ package com.costular.marvelheroes.presentation.heroeslist
 
 import android.app.VoiceInteractor
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.support.v7.graphics.Palette
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.costular.marvelheroes.R
 import com.costular.marvelheroes.domain.model.MarvelHeroEntity
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.item_hero.view.*
 
         /**
@@ -49,12 +57,35 @@ class HeroesListAdapter(val clickListener: Click):  RecyclerView.Adapter<HeroesL
     inner class HeroesViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(item: MarvelHeroEntity) = with(itemView) {
             kotlin.with(itemView) {
-                Picasso.get()
+                Glide.with(context)
+                        .asBitmap()
                         .load(item.photoUrl)
+                        .listener(object : RequestListener<Bitmap> {
+                            override fun onResourceReady(resource: Bitmap?, model: Any?, target: Target<Bitmap>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                                resource?.let { loadColorsFromBitmap(it) }
+                                return false
+                            }
+                            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Bitmap>?, isFirstResource: Boolean): Boolean {
+                                return false
+                            }
+
+                        })
                         .into(heroImage)
 
                 heroTitle.text = item.name
                 setOnClickListener { clickListener(item, heroImage) }
+            }
+        }
+
+        fun loadColorsFromBitmap(bitmap: Bitmap) {
+            with(itemView) {
+                Palette.from(bitmap).generate { palette ->
+                    val vibrant = palette.vibrantSwatch
+                    vibrant?.let {
+                        heroTitle.setBackgroundColor(vibrant.rgb)
+                        heroTitle.setTextColor(vibrant.bodyTextColor)
+                    }
+                }
             }
         }
     }
